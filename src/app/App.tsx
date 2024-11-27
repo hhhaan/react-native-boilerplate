10fc15d96c4db88274131cef7d215ad355afaa59
hhhaan <hhanheon@gmail.com>
2024-11-27 17:07:42 +0900

    routes 설정 수정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Routes } from './routes';
 
 export const App = () => {
     return (
         <SafeAreaProvider>
             <StatusBar barStyle="default" />
+            <Routes />
         </SafeAreaProvider>
     );
 };
